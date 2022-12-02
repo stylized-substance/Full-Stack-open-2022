0cc9cf11deedd0068fd7a9ba1079e13b86f8abe5
stylized-substance_Workmachine <116166387+stylized-substance@users.noreply.github.com>
2022-12-02 10:53:55 +0200

    refactored root component into separate components

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,5 +1,37 @@
 import React, { Component } from 'react';
 
+// New components
+
+const Header = (header) => {
+  return (
+    <div>
+      <h1>{header.course}</h1>
+    </div>
+  )
+}
+
+const Content = (content) => {
+  return (
+    <div>
+      <p>
+        {content.part} {content.exercises}
+      </p>
+    </div>
+  )
+}
+
+const Total = (total) => {
+  return (
+    <div>
+      <p>
+        Number of exercises {total.exercises1 + total.exercises2 + total.exercises3}
+      </p>
+    </div>
+  )
+}
+
+// Root component
+
 const App = () => {
   const course = 'Half stack application development'
   const part1 = 'Fundamentals of React'
@@ -8,20 +40,15 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+  
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3 = {exercises3} />
     </div>
   )
 }
