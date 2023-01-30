146aa53f94f01d60f034d6267bc1b54e86a2c5d9
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-01-30 18:23:57 +0200

    refactoring

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -9,6 +9,35 @@ const Button = ({ handleClick, text }) => {
   )
 }
 
+const Statistics = ({ good, neutral, bad, allClicks}) => {
+  console.log(allClicks.length);
+  const all = good + neutral + bad
+  const average = (good - bad) / allClicks.length
+  const positive = good / all
+  return (
+    <div>
+      <p>
+        Good: {good}
+      </p>
+      <p>
+        Neutral: {neutral}
+      </p>
+      <p>
+        Bad: {bad}
+      </p>
+      <p>
+        All: {all}
+      </p>
+      <p>
+        Average: {average}
+      </p>
+      <p>
+        Positive: {positive}
+      </p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -33,32 +62,13 @@ const App = () => {
 
   return (
     <div>
-      <p>
-        <b>Give feedback</b>
-      </p>
+      <h1>Give feedback</h1>
       <Button handleClick={handleGoodClick} text='Good' />
       <Button handleClick={handleNeutralClick} text='Neutral' />
       <Button handleClick={handleBadClick} text='Bad' />
-      <p>
-        <b>Statistics</b>
-      </p>
-      <p>
-        Good: {good}
-      </p>
-      <p>
-        Neutral: {neutral}
-      </p>
-      <p>
-        Bad: {bad}
-      </p>
-      <p>
-        All: {allClicks.length}
-      </p>
-      <p>
-        Average: {allClicks.reduce((p, c) => p + c, 0) / allClicks.length}
-      </p>
-    </div>
-    
+      <h1>Statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} allClicks={allClicks} />
+    </div>    
   )
 }
 
