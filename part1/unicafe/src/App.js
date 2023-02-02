14577a98970a7cd124e98974b0b1cb6d41b635a0
stylized-substance_Workmachine <116166387+stylized-substance@users.noreply.github.com>
2023-02-02 13:36:34 +0200

    rendered statistics in a HTML table

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -11,9 +11,14 @@ const Button = ({ handleClick, text }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <p>
-      {text} {value}
-    </p>
+    <tr>
+      <td>
+        {text}
+      </td>
+      <td>
+        {value}
+      </td>
+    </tr>
   )
 }
 
@@ -29,13 +34,15 @@ const Statistics = ({ good, neutral, bad, allClicks}) => {
   const average = ((good - bad) / allClicks.length).toFixed(2)
   const positive = (good / all).toFixed(2)
   return (
-    <div>
-      <StatisticLine text='Good' value={good} />
-      <StatisticLine text='Neutral' value={neutral} />
-      <StatisticLine text='Bad' value={bad} />
-      <StatisticLine text='Average' value={average} />
-      <StatisticLine text='Positive' value={positive} />
-    </div>
+      <table>
+        <tbody>
+          <StatisticLine text='Good' value={good} />
+          <StatisticLine text='Neutral' value={neutral} />
+          <StatisticLine text='Bad' value={bad} />
+          <StatisticLine text='Average' value={average} />
+          <StatisticLine text='Positive' value={positive + " %"} />
+        </tbody>
+      </table>
   )
 }
 
