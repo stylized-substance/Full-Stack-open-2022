5019434d7f2e73a555197706912d32ad5e68eb00
stylized-substance_Workmachine <116166387+stylized-substance@users.noreply.github.com>
2023-02-10 11:28:39 +0200

    some progress

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,59 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { useState } from 'react'
+
+const Number = ({ name, key }) => {
+  console.log(key);
+  return (
+  <li key={name}>
+    {name}
+  </li>
+  )
+}
 
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
+  console.log(persons);
   const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      content: newName,
+      important: Math.random() < 0.5,
+      id: persons.length + 1,
+    }
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+    console.log('button clicked, event.target')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
+      <div>debug: {newName}</div>
+      <div>debug: {persons[0].name}</div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <ul>
+        {persons.map(person =>
+          <Number name={person.name} key={person.name} />
+        )}
+      </ul>
       ...
     </div>
   )
