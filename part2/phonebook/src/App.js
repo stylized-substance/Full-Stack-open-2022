0f4f073862495386680351dcbcfc59200e9e4e99
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-02-12 21:09:29 +0200

    added id

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -12,7 +12,7 @@ const Number = ({ name, number }) => {
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '666', id: 1 },
-    { name: 'Test Name', number: '999'}
+    { name: 'Test Name', number: '999', id: 2}
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
