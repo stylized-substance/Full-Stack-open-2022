d47ee4ab1c604c4e43179a8aaffbba6d046e700a
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-02-19 19:13:14 +0200

    removed persons from state

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,12 +6,7 @@ import PersonForm from './components/PersonForm'
 import FilterForm from './components/FilterForm'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterText, setFilterText] = useState('')
@@ -23,10 +18,10 @@ const App = () => {
       .get('http://localhost:3001/persons')
       .then(response => {
         console.log('promise fulfilled')
-        setPersons(response.data)
+        setPersonsToDisplay(response.data)
       })
   }, [])
-  console.log('render', persons.length, 'persons');
+  console.log('render', personsToDisplay.length, 'persons');
 
   const nameArray = persons.map(person => person.name)
 
