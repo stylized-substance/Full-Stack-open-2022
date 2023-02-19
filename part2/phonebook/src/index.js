861fcf37bdc8464b9ec2ead6d033ade14b3f68a7
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-02-19 15:36:56 +0200

    added json-server and database file

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import axios from 'axios'
 
+const promise = axios.get('https://blogs.msdn.microsoft.com/powershell/feed/')
+console.log(promise);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
