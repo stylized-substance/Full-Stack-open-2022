070a6748b4c1c5e28062351a2bdb3b084e0fa141
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-03-03 16:18:35 +0200

    first change

--- part2/countryinfo/src/App.js
+++ part2/countryinfo/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
+
+const App = () => {
+  const [searchText, setSearchText] = useState('')
+  const [countries, setCountries] = useState(null)
+
+  useEffect(() => {
+    axios.get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
+  
+  console.log(countries);
+  
+  const handleSearchChange = (event) => {
+    setSearchText(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Find countries
+      </h1>
+      <form>
+        <input value={searchText} onChange={handleSearchChange} />
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
