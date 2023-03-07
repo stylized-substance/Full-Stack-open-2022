08488acfe24809a51a3cfd61f7af7eef228fecdd
stylized-substance_Workmachine <116166387+stylized-substance@users.noreply.github.com>
2023-03-07 12:21:12 +0200

    flag works

--- part2/countryinfo/src/components/CountryList.js
+++ part2/countryinfo/src/components/CountryList.js
@@ -33,26 +33,36 @@ const CountryList = ({ countriesToShow }) => {
 
     if (countriesToShow.length === 1) {
         const country = countriesToShow[0]
+        const capitals = country.capital.map(value =>
+          <li key={value}>
+            {value}
+          </li>)
         const languageValues = Object.values(country.languages)
         const languageList = languageValues.map(value =>
-          <li>
+          <li key={value}>
             {value}
           </li>
         )
+        const flagURL = country.flags.png
 
         return (
             <div>
                 <h2>
                     {country.name.common}
                 </h2>
-                <b>Capital:</b> {country.capital}
-                <br></br>
+                <hr></hr>
+                <b>Capital:</b>
+                <ul>
+                  {capitals}
+                </ul>
+                <hr></hr>
                 <b>Area:</b> {country.area}
                 <hr></hr>
                 <b>Languages:</b>
                 <ul>
                   {languageList}
                 </ul>
+                <img src={flagURL} alt='Flag image'/>
             </div>
         )
     }
