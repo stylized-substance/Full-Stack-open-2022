ce25ba09e981bba0435ea6da9bd2e72f77fce3a1
stylized-substance_Workmachine <116166387+stylized-substance@users.noreply.github.com>
2023-03-10 08:55:38 +0200

    removed list

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,50 +1,47 @@
 import React from 'react';
 
 const Course = ({ header, courses }) => {
-    console.log(courses);
-    return (
-      <div>
+  return (
+    <div>
       <Header content={header} />
       {courses.map(course =>
         <Content name={course.name} parts={course.parts} />
       )}
-      </div>
-    )
-  }
-  
-  const Header = ({ content }) => {
-    return (
-      <div>
-        <h1>
-          {content}
-        </h1>
-      </div>
-    )
-  }
-  
-  const Content = ({ name, parts }) => {
-    return (
-      <div>
-        <h2>
-          {name}
-        </h2>
-        <ul>
-        {parts.map(part =>
-          <Part name={part.name} exercises={part.exercises} id={part.id} />
-        )}
-        </ul>
-        <b>Total number of exercises </b>
-        {parts.reduce((accumulator, obj) => accumulator + obj.exercises, 0)}
-      </div>
-    )
-  }
-  
-  const Part = ({ name, exercises, id }) => {
-    return (
-      <li key={id}>
-        {name} {exercises}
-      </li>
-    )
-  }
+    </div>
+  )
+}
 
-  export default Course
+const Header = ({ content }) => {
+  return (
+    <div>
+      <h1>
+        {content}
+      </h1>
+    </div>
+  )
+}
+
+const Content = ({ name, parts }) => {
+  return (
+    <div>
+      <h2>
+        {name}
+      </h2>
+      {parts.map(part =>
+        <Part name={part.name} exercises={part.exercises} />
+      )}
+      <b>Total number of exercises </b>
+      {parts.reduce((accumulator, obj) => accumulator + obj.exercises, 0)}
+    </div>
+  )
+}
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  )
+}
+
+export default Course
