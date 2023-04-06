da7e4075693fdb0aa7223ed4e6cfb0d3dfab5c50
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-04-06 09:15:03 +0300

    added favortiteBlog function

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,10 @@
 const dummy = (blogs) => 1;
 
-const totalLikes = (blogs) => blogs.reduce(
-  (previousValue, currentValue) => previousValue + currentValue.likes, 0);
+const totalLikes = (blogs) => blogs.reduce((previous, current) => previous + current.likes, 0);
+
+const favoriteBlog = (blogs) => blogs.reduce(
+  (previous, current) => ((previous.likes > current.likes) ? previous : current),
+);
 
 module.exports = {
   dummy,
