02ccf2e024ff02ff919c47940de72ef47b418b05
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-04-13 12:15:48 +0300

    more test stuff

--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -3,6 +3,24 @@ const supertest = require('supertest');
 const app = require('../app');
 
 const api = supertest(app);
+const Blog = require('../models/blog');
+
+const initialBlogs = [
+  {
+    title: 'test1',
+  },
+  {
+    title: 'test2',
+  },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  let blogObject = new Blog(initialBlogs[0]);
+  await blogObject.save();
+  blogObject = new Blog(initialBlogs[1]);
+  await blogObject.save();
+});
 
 test('blogs are returned as json', async () => {
   await api
@@ -21,8 +39,23 @@ test('there are two blogs', async () => {
   expect(response.body).toHaveLength(2);
 });
 
-test('the first note contains the word test', async () => {
+test('the first note is test1', async () => {
   const response = await api.get('/api/blogs');
 
-  expect(response.body[0].title).toBe('test');
+  expect(response.body[0].title).toBe('test1');
 });
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(initialBlogs.length)
+});
+
+test('a specific blog is within the returned blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  const contents = response.body.map(r => r.title)
+  expect(contents).toContain(
+    'test1',
+  )
+})
