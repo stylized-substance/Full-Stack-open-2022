a744f0dc1232753607d5ff20d30ff6514a9876ae
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-04-18 09:44:09 +0300

    add commas

--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -4,19 +4,19 @@ const blogSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
-  }
+  },
   author: {
     type: String,
     required: true,
-  }
+  },
   url: {
     type: String,
     required: true,
-  }
+  },
   likes: {
     type: Number,
     default: 0,
-  }
+  },
 });
 
 module.exports = mongoose.model('Blog', blogSchema);
