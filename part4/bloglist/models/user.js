08c6cd0106445ceed7608a20341098a903f1e991
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-04-19 08:33:53 +0300

    added user.js

---
+++ part4/bloglist/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  name: String,
+  passwordHash: String,
+  notes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog'
+    }
+  ],
+})
+
+userSchema.set('toJson', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject._v
+    delete returnedObject.passwordHash
+  }
+})
+
+const User = mongoosel.model('User, userSchema')
+
+module.exports = User
