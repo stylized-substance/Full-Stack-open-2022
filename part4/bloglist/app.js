1147d1a9384e76eab7c667b966ea6d9cb3b6efa4
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-04-19 08:44:36 +0300

    took usersRouter into use

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -9,6 +9,7 @@ require('express-async-errors')
 const app = express();
 
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('../controllers/users')
 const logger = require('./utils/logger');
 const Blog = require('./models/blog');
 
@@ -28,6 +29,7 @@ app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter)
 
 module.exports = app;
 
