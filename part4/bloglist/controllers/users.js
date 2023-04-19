d3a8d3f63c8e09f6bb2a7c9b46560762ceb83fde
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-04-19 11:16:32 +0300

    modified POST route

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -1,31 +1,32 @@
-const bcrypt = require('bcrypt')
-const usersRouter = require('express').Router()
-const User = require('../models/user')
+const bcrypt = require('bcrypt');
+const usersRouter = require('express').Router();
+const User = require('../models/user');
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
-  response.json(users)
-})
+  const users = await User.find({});
+  response.json(users);
+});
 
 usersRouter.post('/', async (request, response) => {
-  const { username, name, password } = request.body
+  const { username, name, password } = request.body;
 
   if (password.length < 3) {
-    response.status(400).send('password must be at least 3 characters long')
+    response.status(400).send('password must be at least 3 characters long');
+    return;
   }
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
 
   const user = new User({
     username,
     name,
     passwordHash,
-  })
+  });
 
-  const savedUser = await user.save()
+  const savedUser = await user.save();
 
-  response.status(201).json(savedUser)
-})
+  response.status(201).json(savedUser);
+});
 
-module.exports = usersRouter
+module.exports = usersRouter;
