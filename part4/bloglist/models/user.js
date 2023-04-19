b36a61e65bdf512975f06c1235632c3e076b487f
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-04-19 10:40:49 +0300

    fix userSchema

--- part4/bloglist/models/user.js
+++ part4/bloglist/models/user.js
@@ -19,11 +19,11 @@ const userSchema = new mongoose.Schema({
 
 userSchema.plugin(uniqueValidator);
 
-userSchema.set('toJson', {
+userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
-    delete returnedObject._v;
+    delete returnedObject.__v;
     delete returnedObject.passwordHash;
   },
 });
