c234453af57304bd7ccf256c930e63cf4a39ff70
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-06-05 11:03:41 +0300

    anecdotes are sorted by votes

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -3,6 +3,7 @@ import { voteAction, addNew } from './reducers/anecdoteReducer'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
+  const sortedByVotes = anecdotes.sort((a, b) => b.votes - a.votes)
   const dispatch = useDispatch()
 
   const vote = (id) => {
@@ -21,7 +22,7 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {sortedByVotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
