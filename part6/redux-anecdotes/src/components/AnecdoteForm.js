656a55abc7ea091236def154f4a83614e7e528df
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-06-05 11:46:16 +0300

    created separate anecdote list component

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,4 +1,4 @@
-import { useSelector, useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { addNew } from '../reducers/anecdoteReducer'
 
 const AnecdoteForm = () => {
