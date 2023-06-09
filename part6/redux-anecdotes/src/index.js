11b975b9b4d7a2f54a93b4e5a528aa7816b87bbd
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-06-09 14:15:55 +0300

    Revert "redux toolkit stuff"
    This reverts commit e82159a7c3ffe326ce3f0a95f87ac1daada25299.

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
-import { configureStore } from '@reduxjs/toolkit'
 import App from './App'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 
-const store = configureStore({
-  redurer: {
-    notes: anecdoteReducer,
-    filter: filterReducer
-  }
+const combinedReducer = combineReducers({
+  anecdotes: anecdoteReducer,
+  filter: filterReducer
 })
+
+const store = createStore(combinedReducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,5 +1,3 @@
-import { createSlice } from '@reduxjs/toolkit'
-
 const anecdotesAtStart = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
@@ -20,20 +18,6 @@ const asObject = (anecdote) => {
 }
 
 const initialState = anecdotesAtStart.map(asObject)
-
-const anecdoteSlice = createSlice({
-  name: 'anecdotes',
-  initialState,
-  reducers: {
-    createAnecdote(state, action) {
-      const content = action.payload
-      state.push({
-        content,
-        id: getId()
-      })
-    }
-  }
-})
 
 const anecdoteReducer = (state = initialState, action) => {
   console.log('state now: ', state)
