7702fa25b0749ff47e832b76c015d2f5ff6bc9ab
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-06-12 10:38:34 +0300

    moved store creation to store.js

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,17 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
-import anecdoteReducer from './reducers/anecdoteReducer'
-import filterReducer from './reducers/filterReducer'
-
-const combinedReducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  filter: filterReducer
-})
-
-const store = createStore(combinedReducer)
+import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
