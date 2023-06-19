95b0caf42268356001aa462538d3ba5959d1bca8
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-06-19 11:25:05 +0300

    added effect hook dependency

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -12,7 +12,7 @@ const App = () => {
   useEffect(() => {
     anecdoteService
       .getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
-  }, [])
+  }, [dispatch])
 
   return (
     <div>
