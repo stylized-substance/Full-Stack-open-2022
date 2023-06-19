9a63abd71639dba0fea689fa7c28b0f50b1da807
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-06-19 10:51:42 +0300

    moved services folder, app fetches anecdotes from DB

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
 import store from './store'
+import anecdotes from './services/anecdotes'
+import anecdoteReducer, { appendAnecdote } from './reducers/anecdoteReducer'
+
+anecdotes.getAll().then(anecdotes =>
+  anecdotes.forEach(anecdote => {
+    store.dispatch(appendAnecdote(anecdote))
+  })
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
