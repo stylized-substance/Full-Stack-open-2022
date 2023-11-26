7cfb7149323d0365ccc5033932e92c45596da5c7
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-11-26 18:17:13 +0200

    removed rules that conflict with prettier

--- part7/bloglist-frontend_redux-notifications/.eslintrc.js
+++ part7/bloglist-frontend_redux-notifications/.eslintrc.js
@@ -22,30 +22,11 @@ module.exports = {
     'react', 'jest', 'cypress'
   ],
   'rules': {
-    'indent': [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
     'quotes': [
       0,
       'single'
     ],
-    'semi': [
-      'error',
-      'never'
-    ],
     'eqeqeq': 'error',
-    'no-trailing-spaces': 'error',
-    'object-curly-spacing': [
-      'error', 'always'
-    ],
-    'arrow-spacing': [
-      'error', { 'before': true, 'after': true }
-    ],
     'no-console': 0,
     'react/prop-types': 0,
     'react/react-in-jsx-scope': 'off'
