645645281b80e9f5970661139d42ee384288e7ed
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-12-08 14:05:09 +0200

    refactor

--- part7/bloglist-frontend_redux-notifications/src/components/SingleUserView.js
+++ part7/bloglist-frontend_redux-notifications/src/components/SingleUserView.js
@@ -5,32 +5,32 @@ const SingleUserView = ({ users }) => {
   const userId = useParams().id
   const user = users.find((user) => user.id === userId)
 
-  if (user) {
-    const blogsMapped = user.blogs.map((blog) => (
-      <tr key={blog.id}>
-        <td>{blog.title}</td>
-      </tr>
-    ))
-
-    return (
-      <div>
-        <br></br>
-        <h2>{user.username}</h2>
-        <Table>
-          <thead>
-            <tr>
-              <th>
-                <h3>Added blogs</h3>
-              </th>
-            </tr>
-          </thead>
-          <tbody>{blogsMapped}</tbody>
-        </Table>
-      </div>
-    )
-  } else {
-    ;('user is empty')
+  if (!user) {
+    return null
   }
+
+  const blogsMapped = user.blogs.map((blog) => (
+    <tr key={blog.id}>
+      <td>{blog.title}</td>
+    </tr>
+  ))
+
+  return (
+    <div>
+      <br></br>
+      <h2>{user.username}</h2>
+      <Table>
+        <thead>
+          <tr>
+            <th>
+              <h3>Added blogs</h3>
+            </th>
+          </tr>
+        </thead>
+        <tbody>{blogsMapped}</tbody>
+      </Table>
+    </div>
+  )
 }
 
 export default SingleUserView
