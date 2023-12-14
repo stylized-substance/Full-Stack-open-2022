3a4e9d3f82e3bcb27879628c41ee1e6d3fc86864
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2023-12-14 12:40:57 +0200

    converted notifications to react-bootstrap

--- part7/bloglist-frontend_redux-notifications/src/components/Notification.js
+++ part7/bloglist-frontend_redux-notifications/src/components/Notification.js
@@ -1,48 +1,25 @@
 import PropTypes from 'prop-types'
 import { useSelector } from 'react-redux'
+import Alert from 'react-bootstrap/Alert'
 
 const Notification = () => {
   const notificationState = useSelector((state) => state.notification)
   const message = notificationState.content
   const notificationType = notificationState.type
 
-  const successStyle = {
-    color: 'green',
-    background: 'lightgrey',
-    fontSize: '20px',
-    borderStyle: 'solid',
-    borderRadius: '5px',
-    padding: '10px',
-    marginBottom: '10px'
-  }
-
-  const errorStyle = {
-    color: 'red',
-    background: 'lightgrey',
-    fontSize: '20px',
-    borderStyle: 'solid',
-    borderRadius: '5px',
-    padding: '10px',
-    marginBottom: '10px'
-  }
-
-  if (message === null) {
-    return null
-  }
-
   if (notificationType === 'success') {
     return (
-      <div style={successStyle} className="success">
+      <Alert key='success' variant='success'>
         {message}
-      </div>
+      </Alert>
     )
   }
 
   if (notificationType === 'error') {
     return (
-      <div style={errorStyle} className="error">
+      <Alert key='danger' variant='danger'>
         {message}
-      </div>
+      </Alert>
     )
   }
 }
