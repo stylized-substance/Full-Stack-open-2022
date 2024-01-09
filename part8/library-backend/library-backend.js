8fb7cd52592e76d1f56b6e188b9a6c023a773cfb
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-01-09 14:19:25 +0200

    exercise 8.3 done

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -105,10 +105,18 @@ const typeDefs = `
     id: String!
   }
 
+  type Author {
+    name: String!
+    id: String!
+    born: Int
+    bookCount: Int
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `;
 
@@ -116,8 +124,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
   },
+  Author: {
+    bookCount: (root) => books.filter(book => root.name === book.author).length
+  }
 };
 
 const server = new ApolloServer({
