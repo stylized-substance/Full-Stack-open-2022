11dc740ecb10eb5858319ca4ea7901255052faba
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-01-10 09:05:33 +0200

    exercise 8.4 done

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -115,7 +115,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -124,7 +124,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books
+      } else {
+        return books.filter(book => book.author === args.author)
+      }
+    },
     allAuthors: () => authors
   },
   Author: {
