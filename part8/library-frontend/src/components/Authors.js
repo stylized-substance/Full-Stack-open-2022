442ec03577c7e8d7327d44f29411528f71e49db1
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-01-11 11:48:08 +0200

    exercise 8.8 done

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,8 +1,17 @@
+import { ALL_AUTHORS } from "../queries"
+import { useQuery } from "@apollo/client"
+
 const Authors = (props) => {
+  const result = useQuery(ALL_AUTHORS)
+  
+  if (result.loading) {
+    return <div>loading..</div>
+  }
   if (!props.show) {
     return null
   }
-  const authors = []
+
+  const authors = result.data.allAuthors
 
   return (
     <div>
