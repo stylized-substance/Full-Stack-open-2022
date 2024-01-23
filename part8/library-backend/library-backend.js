081a46c6dffdb7d9f0a1ef9c07cbf6372954f9ea
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-01-23 18:16:37 +0200

    update

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -24,101 +24,6 @@ mongoose.connect(MONGODB_URI)
     console.log('error connecting to MongoDB:', error.message)
   })
 
-let authors = [
-  {
-    name: "Robert Martin",
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: "Martin Fowler",
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963,
-  },
-  {
-    name: "Fyodor Dostoevsky",
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821,
-  },
-  {
-    name: "Joshua Kerievsky", // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  {
-    name: "Sandi Metz", // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-];
-
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conección con el libro
- */
-
-let books = [
-  {
-    title: "Clean Code",
-    published: 2008,
-    author: "Robert Martin",
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring"],
-  },
-  {
-    title: "Agile software development",
-    published: 2002,
-    author: "Robert Martin",
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ["agile", "patterns", "design"],
-  },
-  {
-    title: "Refactoring, edition 2",
-    published: 2018,
-    author: "Martin Fowler",
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring"],
-  },
-  {
-    title: "Refactoring to patterns",
-    published: 2008,
-    author: "Joshua Kerievsky",
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring", "patterns"],
-  },
-  {
-    title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
-    published: 2012,
-    author: "Sandi Metz",
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring", "design"],
-  },
-  {
-    title: "Crime and punishment",
-    published: 1866,
-    author: "Fyodor Dostoevsky",
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ["classic", "crime"],
-  },
-  {
-    title: "The Demon ",
-    published: 1872,
-    author: "Fyodor Dostoevsky",
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ["classic", "revolution"],
-  },
-];
-
-/*
-  you can remove the placeholder query once your first one has been implemented 
-*/
 
 const typeDefs = `
   type Book {
@@ -190,39 +95,11 @@ const resolvers = {
       return authors.length
     },
     allBooks: async (root, args) => {
-      // if (!args.author && !args.genre) {
-      //   return books
-      // }
-      // if (args.author) {
-      //   const booksByAuthor = books.filter(book => book.author === args.author)
-      //   if (args.genre) {
-      //     return booksByAuthor.filter(book => book.genres.includes(args.genre))
-      //   } else {
-      //     return booksByAuthor
-      //   }
-      // }
-      // if (args.genre) {
-      //   return books.filter(book => book.genres.includes(args.genre))
-      // }
       let books = await Book.find({})
       for (let book of books) {
         const author = await Author.findById(book.author)
         book.author = author
       }
-      // const authors = await Author.find({})
-      // const booksWithAuthors = books.map((book) => {
-      //   const author = authors.find((author) => book.id === author.id)
-      //   console.log(author)
-      //   book.author = author
-      //   return book
-      // })
-      // let booksWithAuthors = books.map(async (book) => {
-      //   const author = await Author.findById(book.author)
-      //   book.author = author
-      //   return book
-      // })
-      // booksWithAuthors = Promise.all(booksWithAuthors).then((values) => {return values})
-      // console.log(booksWithAuthors)
       return books
     },
     allAuthors: async () => {
@@ -235,7 +112,6 @@ const resolvers = {
     },
   },
   Author: {
-    //bookCount: (root) => books.filter(book => root.name === book.author).length
     bookCount: async (root) => {
       const books = await Book.find({ author: root.id })
       return books.length
@@ -310,8 +186,9 @@ const resolvers = {
       }
     },
     createUser: async (root, args) => {
-      const user = new User({ username: args.username })
-      user.save()
+      console.log({ ...args })
+      const user = new User({ ...args })
+      return user.save()
         .catch((error) => {
           throw new GraphQLError('Creating new user failed', {
             extensions: {
@@ -331,14 +208,10 @@ const resolvers = {
           }
         })
       }
-
       const userForToken = {
         username: user.username,
         id: user._id
       }
-
-      console.log(userForToken)
-
       return { value: jwt.sign(userForToken, process.env.JWT_SECRET) }
     }
   }
@@ -356,14 +229,8 @@ startStandaloneServer(server, {
     if (auth && auth.startsWith('Bearer ')) {
       const decodedToken = jwt.verify(
         auth.substring(7), process.env.JWT_SECRET
-        )
-      console.log(decodedToken)
-      // console.log(auth)
-      const currentUser = await User.findById(decodedtoken.id)
-      User.findById(decodedtoken.id)
-        .catch((error) => {
-          console.log(error)
-        })
+      )
+      const currentUser = await User.findById(decodedToken.id)
       return currentUser
     }
   }
