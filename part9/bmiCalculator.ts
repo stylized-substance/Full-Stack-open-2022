5d0d7f4fff717b4ec5892f358506671bd5b0dfc0
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-02-05 18:55:14 +0200

    bmiCalculator works with cmd line arguments

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,15 +1,43 @@
 const calculateBmi = (height: number, weight: number): string => {
-  const bmi = weight / ((height * 0.01) ** 2)
+  const bmi = weight / ((height * 0.01) ** 2);
   switch(true) {
     case bmi < 18.5:
-      return 'Underweight'
+      return 'Underweight';
     case bmi < 25:
-      return 'Normal (healthy weight)'
+      return 'Normal (healthy weight)';
     case bmi < 30:
-      return 'Overweight'
+      return 'Overweight';
     case bmi >= 30:
-      return 'Obese'
+      return 'Obese';
   }
 }
 
-console.log(calculateBmi(180, 74))
+interface BmiValues {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: string[]): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Only numbers are allowed as arguments');
+  }
+}
+
+try {
+ const { height, weight } = parseArguments(process.argv);
+ console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+  let errorMessage = 'Error: ';
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}
