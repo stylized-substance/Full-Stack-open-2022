46064860f26526b4070c619500e0e9d48c0c635d
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-02-29 15:04:24 +0200

    exercise 9.23 done

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -45,10 +45,10 @@ export type Entry = OccupationalHealthcareEntry | HospitalEntry | HealthCheck;
 export interface Patient {
   id: string;
   name: string;
-  dateOfBirth: string;
-  ssn: string;
-  gender: string;
   occupation: string;
+  gender: string;
+  ssn: string;
+  dateOfBirth: string;
   entries?: Entry[];
 }
 
